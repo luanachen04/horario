37d3e0101790cd1ea31c2a9034789116ffcc791c
luanachen04 <116042113+luanachen04@users.noreply.github.com>
2024-05-30 04:47:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,333 +20,273 @@ function changeDay() {
     scheduleBody.innerHTML = "";
 
     if (selectedDay === "dias-uteis") {
-        if (departure === "terminal") {
+        if (departure === "terminalmarilia") {
             scheduleBody.innerHTML += `
                 <tr>
-                    <td>07:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>05:40</td>
+                    <td>06:40</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>07:45</td>
-                    <td>Terminal Urbano</td>
+                    <td>06:40</td>
+                    <td>07:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>08:35</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>09:25</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>10:15</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>11:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>11:50</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>12:40</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>13:30</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>14:20</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>15:10</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>16:10</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>17:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>17:50</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>18:40</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>20:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>21:30</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>23:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>23:50</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-            `;
-        } else if (departure === "castelobranco") {
-            scheduleBody.innerHTML += `
-                <tr>
-                    <td>06:35</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>07:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>08:10</td>
-                    <td>Castelo Branco</td>
+                    <td>07:30</td>
+                    <td>08:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
                     <td>09:00</td>
-                    <td>Castelo Branco</td>
+                    <td>10:00</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>09:50</td>
-                    <td>Castelo Branco</td>
+                    <td>11:30</td>
+                    <td>12:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>10:40</td>
-                    <td>Castelo Branco</td>
+                    <td>13:00</td>
+                    <td>14:00</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>11:25</td>
-                    <td>Castelo Branco</td>
+                    <td>14:30</td>
+                    <td>15:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>12:15</td>
-                    <td>Castelo Branco</td>
+                    <td>16:00</td>
+                    <td>17:00</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>13:05</td>
-                    <td>Castelo Branco</td>
+                    <td>17:00</td>
+                    <td>18:00</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>13:55</td>
-                    <td>Castelo Branco</td>
+                    <td>17:20</td>
+                    <td>18:20</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>14:45</td>
-                    <td>Castelo Branco</td>
+                    <td>18:30</td>
+                    <td>19:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>15:35</td>
-                    <td>Castelo Branco</td>
+                    <td>19:30</td>
+                    <td>20:30</td>
+                    <td>Terminal Marilia</td>
+                </tr>
+                
+                
+            `;
+        } else if (departure === "terminalgarca") {
+            scheduleBody.innerHTML += `
+                <tr>
+                    <td>05:40</td>
+                    <td>06:40</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>16:35</td>
-                    <td>Castelo Branco</td>
+                    <td>06:40</td>
+                    <td>07:40</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>17:25</td>
-                    <td>Castelo Branco</td>
+                    <td>06:50</td>
+                    <td>07:40</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>18:15</td>
-                    <td>Castelo Branco</td>
+                    <td>07:40</td>
+                    <td>08:40</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>19:05</td>
-                    <td>Castelo Branco</td>
+                    <td>08:30</td>
+                    <td>09:30</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>20:20</td>
-                    <td>Castelo Branco</td>
+                    <td>10:30</td>
+                    <td>11:30</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>21:20</td>
-                    <td>Castelo Branco</td>
+                    <td>12:00</td>
+                    <td>13:00</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>21:50</td>
-                    <td>Castelo Branco</td>
+                    <td>14:30</td>
+                    <td>15:30</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>23:20</td>
-                    <td>Castelo Branco</td>
+                    <td>15:30</td>
+                    <td>16:30</td>
+                    <td>Terminal Garça</td>
+                </tr>
+                <tr>
+                    <td>16:30</td>
+                    <td>17:20</td>
+                    <td>Terminal Garça</td>
+                </tr>
+                <tr>
+                    <td>17:30</td>
+                    <td>18:30</td>
+                    <td>Terminal Garça</td>
+                </tr>
+                <tr>
+                    <td>18:30</td>
+                    <td>19:30</td>
+                    <td>Terminal Garça</td>
                 </tr>
             `;
         }
     } else if (selectedDay === "sabado") {
-        if (departure === "terminal") {
+        if (departure === "terminalmarilia") {
             scheduleBody.innerHTML += `
                 <tr>
-                    <td>07:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>05:40</td>
+                    <td>06:40</td>
+                    <td>Terminal Marilia</td>
+                </tr>
+                <tr>
+                    <td>07:30</td>
+                    <td>08:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
                     <td>09:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>10:00</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>11:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>12:30</td>
+                    <td>13:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>13:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>14:30</td>
+                    <td>15:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>15:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>17:30</td>
+                    <td>18:30</td>
+                    <td>Terminal Marilia</td>
                 </tr>
                 <tr>
-                    <td>16:10</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>18:05</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>20:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>22:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
+                    <td>19:30</td>
+                    <td>20:30</td>
+                    <td>Terminal Marilia</td>
 
             `;
-        } else if (departure === "castelobranco") {
+        } else if (departure === "terminalgarca") {
             scheduleBody.innerHTML += `
                 <tr>
-                    <td>06:05</td>
-                    <td>Castelo Branco</td>
+                    <td>05:40</td>
+                    <td>06:40</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>07:25</td>
-                    <td>Castelo Branco</td>
+                    <td>07:40</td>
+                    <td>08:40</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>09:25</td>
-                    <td>Castelo Branco</td>
+                    <td>09:00</td>
+                    <td>10:00</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>11:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>13:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>15:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>16:35</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>18:30</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>20:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>22:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-            `;
-        }
-    } else if (selectedDay === "domingo") {
-        if (departure === "terminal") {
-            scheduleBody.innerHTML += `
-                <tr>
-                    <td>05:30</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>07:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>10:30</td>
+                    <td>11:30</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
                     <td>12:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>13:00</td>
+                    <td>Terminal Garça</td>
+                </tr>
+                <tr>
+                    <td>15:30</td>
+                    <td>16:30</td>
+                    <td>Terminal Garça</td>
+                </tr>
+                <tr>
+                    <td>17:30</td>
+                    <td>18:30</td>
+                    <td>Terminal Garça</td>
+            `;
+        }
+    } else if (selectedDay === "domingo") {
+        if (departure === "terminalmarilia") {
+            scheduleBody.innerHTML += `
+                <tr>
+                    <td>07:30</td>
+                    <td>08:30</td>
+                    <td>Terminal Marilia</td>
+                </tr>
+                <tr>
+                    <td>11:00</td>
+                    <td>12:00</td>
+                    <td>Terminal Marilia</td>
+                </tr>
+                <tr>
+                    <td>16:00</td>
+                    <td>17:00</td>
+                    <td>Terminal Marilia</td>
+                </tr>
+                <tr>
+                    <td>19:30</td>
+                    <td>20:30</td>
+                    <td>Terminal Marilia</td>
+                </tr>
+            `;
+        } else if (departure === "terminalgarca") {
+            scheduleBody.innerHTML += `
+                <tr>
+                    <td>05:40</td>
+                    <td>06:40</td>
+                    <td>Terminal Garça</td>
+                </tr>
+                <tr>
+                    <td>09:00</td>
+                    <td>10:00</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
                     <td>15:00</td>
-                    <td>Terminal Urbano</td>
+                    <td>16:00</td>
+                    <td>Terminal Garça</td>
                 </tr>
                 <tr>
-                    <td>16:10</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>18:05</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>20:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>22:00</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-                <tr>
-                    <td>23:50</td>
-                    <td>Terminal Urbano</td>
-                </tr>
-            `;
-        } else if (departure === "castelobranco") {
-            scheduleBody.innerHTML += `
-                <tr>
-                    <td>05:55</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>07:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>09:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>12:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>15:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>16:35</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
+                    <td>17:30</td>
                     <td>18:30</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>20:25</td>
-                    <td>Castelo Branco</td>
-                </tr>
-                <tr>
-                    <td>22:25</td>
-                    <td>Castelo Branco</td>
+                    <td>Terminal Garça</td>
                 </tr>
             `;
         }
     }
 }
+
+document.getElementById("visualizarMapaBtn").addEventListener("click", function() {
+    var mapaUrl = "https://www.google.com/maps/d/u/0/embed?mid=1MT5YhuN0elixCclPpX9O1FJU4tosKow&ehbc=2E312F&noprof=1";
+    var mapaContainer = document.getElementById("mapa-container");
+    var mapaIframe = document.getElementById("mapa-iframe");
+
+    if (mapaContainer.style.display === "block") {
+        mapaContainer.style.display = "none";
+        mapaIframe.src = "";
+    } else {
+        mapaIframe.src = mapaUrl;
+        mapaContainer.style.display = "block";
+    }
+});
